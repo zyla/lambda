35e433df0a1b9007b7bb2769f6ebe3e6a3689372
Maciej Bielecki <zyla@prati.pl>
2020-09-19 09:04:10 +0200

    Some HM inference

--- src/hm.rs
+++ src/hm.rs
@@ -13,10 +13,19 @@ pub enum Type<'a> {
     Fun(&'a Type<'a>, &'a Type<'a>),
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)] // FIXME: we're Cloning vectors
 pub struct PolyType<'a> {
     foralls: Vec<TyVar>,
     ty: &'a Type<'a>,
+}
+
+impl<'a> PolyType<'a> {
+    fn from_ty(ty: &'a Type<'a>) -> Self {
+        PolyType {
+            foralls: vec![],
+            ty,
+        }
+    }
 }
 
 pub type Id = usize;
@@ -42,16 +51,7 @@ pub enum Error<'a> {
     },
 }
 
-pub type Context<'a> = context::Context<Id, &'a PolyType<'a>>;
-
-pub fn infer<'a>(
-    arena: &'a Arena<Type<'a>>,
-    ctx: &Context<'a>,
-    term: &'a Term<'a>,
-) -> Result<&'a Type<'a>, Error<'a>> {
-    let mut ctx = ctx.clone();
-    infer_mut(arena, &mut ctx, term)
-}
+pub type Context<'a> = context::Context<Id, PolyType<'a>>;
 
 struct TyEnv<'a> {
     tyvar_union: Vec<TyVar>,
@@ -77,7 +77,7 @@ impl<'a> TyEnv<'a> {
 fn instantiate<'a>(
     arena: &'a Arena<Type<'a>>,
     tyenv: &mut TyEnv,
-    polyty: &'a PolyType<'a>,
+    polyty: &PolyType<'a>,
 ) -> &'a Type<'a> {
     let mapping: HashMap<TyVar, TyVar> = polyty
         .foralls
@@ -112,12 +112,90 @@ fn subst<'a>(
     }
 }
 
-pub fn infer_mut<'a>(
+fn generalize<'a>(arena: &'a Arena<Type<'a>>, tyenv: &mut TyEnv, ty: &'a Type<'a>) -> PolyType<'a> {
+    let mut mapping = HashMap::new();
+    let new_ty = generalize_mut(arena, tyenv, &mut mapping, ty);
+    PolyType {
+        foralls: mapping.values().copied().collect(),
+        ty: new_ty,
+    }
+}
+
+/// Find free variables and replace them with ones found in the `mapping`.
+/// If a tyvar is not found in the mapping, then a fresh one is created and inserted into
+/// `mapping`.
+fn generalize_mut<'a>(
+    arena: &'a Arena<Type<'a>>,
+    tyenv: &mut TyEnv,
+    mapping: &mut HashMap<TyVar, TyVar>,
+    ty: &'a Type<'a>,
+) -> &'a Type<'a> {
+    match *ty {
+        Type::Int => ty,
+        Type::Bool => ty,
+        Type::Var(tv) => {
+            let new_tv = match mapping.get(&tv) {
+                Some(tv) => *tv,
+                None => {
+                    let new_tv = tyenv.fresh();
+                    mapping.insert(tv, new_tv);
+                    new_tv
+                }
+            };
+            arena.alloc(Type::Var(new_tv))
+        }
+        Type::Fun(arg, ret) => {
+            let new_arg = generalize_mut(arena, tyenv, mapping, arg);
+            let new_ret = generalize_mut(arena, tyenv, mapping, ret);
+            // avoid allocation if the type is unchanged
+            if ptr::eq(arg, new_arg) && ptr::eq(ret, new_ret) {
+                ty
+            } else {
+                arena.alloc(Type::Fun(new_arg, new_ret))
+            }
+        }
+    }
+}
+
+pub fn infer<'a>(
+    arena: &'a Arena<Type<'a>>,
+    ctx: &Context<'a>,
+    term: &'a Term<'a>,
+) -> Result<PolyType<'a>, Error<'a>> {
+    let mut ctx: Context<'a> = ctx.clone();
+    let mut tyenv = TyEnv::new();
+    let ty = infer_mut(arena, &mut ctx, &mut tyenv, term)?;
+    Ok(generalize(arena, &mut tyenv, ty))
+}
+
+fn infer_mut<'a>(
     arena: &'a Arena<Type<'a>>,
     ctx: &mut Context<'a>,
+    tyenv: &mut TyEnv<'a>,
     term: &'a Term<'a>,
 ) -> Result<&'a Type<'a>, Error<'a>> {
-    unimplemented!();
+    match *term {
+        Term::Var(id) => {
+            let ty = ctx.get(&id).ok_or(Error::UnknownVariable(id))?;
+            Ok(instantiate(arena, tyenv, ty))
+        }
+        Term::Let(id, def, body) => {
+            let var_ty = infer_mut(arena, ctx, tyenv, def)?;
+            let var_polyty = generalize(arena, tyenv, var_ty);
+            infer_mut(arena, &mut ctx.with(id, var_polyty), tyenv, body)
+        }
+        Term::Lam(arg_id, body) => {
+            let arg_ty = arena.alloc(Type::Var(tyenv.fresh()));
+            let result_ty = infer_mut(
+                arena,
+                &mut ctx.with(arg_id, PolyType::from_ty(arg_ty)),
+                tyenv,
+                body,
+            )?;
+            Ok(arena.alloc(Type::Fun(arg_ty, result_ty)))
+        }
+        Term::App(f, x) => unimplemented!(),
+    }
 }
 
 #[cfg(test)]
